3919a0cf3b18a947a7e362fee61ae50d55c5913d
henderjon <list@box370.net>
2020-06-03 11:52:59 -0500

    fixes example

--- example/main.go
+++ example/main.go
@@ -9,18 +9,18 @@ import (
 )
 
 func main() {
-	fmt.Println("OID\t", uid.OID())
-	fmt.Println("UID\t", uid.UID())
+	fmt.Println("OID\t", oid.OID())
+	fmt.Println("UID\t", oid.UID())
 
-	hex := uid.NewGenerator(uid.HexEncoder, uid.MathSource, 8)
+	hex := oid.NewGenerator(oid.HexEncoder, oid.MathSource, 8)
 	fmt.Println("hex OID\t", hex.OID())
 	fmt.Println("hex UID\t", hex.UID())
 
-	b32 := uid.NewGenerator(base32.StdEncoding, uid.MathSource, 5)
+	b32 := oid.NewGenerator(base32.StdEncoding, oid.MathSource, 5)
 	fmt.Println("base32 OID\t", b32.OID())
 	fmt.Println("base32 UID\t", b32.UID())
 
-	b64 := uid.NewGenerator(base64.StdEncoding.WithPadding(base64.NoPadding), uid.MathSource, 6)
+	b64 := oid.NewGenerator(base64.StdEncoding.WithPadding(base64.NoPadding), oid.MathSource, 6)
 	fmt.Println("base64 OID\t", b64.OID())
 	fmt.Println("base64 UID\t", b64.UID())
 }
