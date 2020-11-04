cf4958f59ba22cfb7a5ab64d9a468b69e2870e3b
henderjon <list@box370.net>
2020-11-04 11:36:55 -0600

    doc is now man

--- Makefile
+++ Makefile
@@ -1,13 +1,22 @@
+################################################################################
+#### INSTALLATION VARS
+################################################################################
+PREFIX=/usr/local
+
+################################################################################
+#### BUILD VARS
+################################################################################
 BIN=oid
-CMD=cmd
-BINS=release
+BINS_DIR=bin
+CMD_DIR=cmd
 HEAD=$(shell git describe --dirty --long --tags 2> /dev/null  || git rev-parse --short HEAD)
-TIMESTAMP=$(shell date -u '+%Y-%m-%dT%H:%M:%S %z %Z')
+TIMESTAMP=$(shell TZ=UTC date '+%FT%T %Z')
 DEPLOYMENT_PATH=apps/$(BIN)-$(HEAD)
+SSH_ALIAS=
 
 LDFLAGS="-X 'main.buildVersion=$(HEAD)' -X 'main.buildTimestamp=$(TIMESTAMP)' -X 'main.compiledBy=$(shell go version)'" # `-s -w` removes some debugging info that might not be necessary in production (smaller binaries)
 
-all: build
+all: local
 
 ################################################################################
 #### HOUSE CLEANING
@@ -17,18 +26,37 @@ all: build
 dep:
 	go mod vendor
 
+.PHONY: check
+check:
+	golint
+	goimports -w ./
+	gofmt -w ./
+	go vet
+
 .PHONY: clean
 clean:
-	rm -f $(BIN) $(BINS)/$(BIN)
+	rm -f $(BIN) $(BIN)-* $(BINS_DIR)/$(BIN) $(BINS_DIR)/$(BIN)-*
+
+################################################################################
+#### INSTALL
+################################################################################
+
+.PHONY: install
+install:
+	mkdir -p $(PREFIX)/bin
+	cp $(BINS_DIR)/$(BIN) $(PREFIX)/bin/$(BIN)
+
+.PHONY: uninstall
+uninstall:
+	rm -f $(PREFIX)/bin/$(BIN)
 
 ################################################################################
 #### ENV BUILDS
 ################################################################################
 
-.PHONY: build
-build: clean
-	go build -ldflags $(LDFLAGS) -o $(BIN) ./$(CMD)/$(BIN)
+.PHONY: local
+local: clean
+	go build -ldflags $(LDFLAGS) -o $(BINS_DIR)/$(BIN) ./$(CMD_DIR)/$(BIN)
 
-.PHONY: install
-install:
-	go install -ldflags $(LDFLAGS) ./$(CMD)/$(BIN)
+
+
--- cmd/oid/main.go
+++ cmd/oid/main.go
@@ -1,30 +1,39 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"os"
+	"text/template"
 
 	"github.com/henderjon/oid"
 )
 
-const doc = `
-%s is a simple utility for generating un/ordered IDs.
+// Tmpl is a basic man page[-ish] looking template
+const Tmpl = `
+{{define "manual"}}
+NAME
+  {{.Bin}} - a simple utility for generating un/ordered IDs.
 
-By default, it is the base 32 encoding of a binary encoded string
-comprising an 8 byte nanosecond precision unix timestamp and an 8 byte random
-number, in that order. The timestamp prefix allows these IDs to be ordered.
+SYNOPSIS
+  {{.Bin}} [-len=8] [-hex] [-secure] [-uid]
 
-Using '-uid' makes it a base 32 encoding of a binary encoded string
-comprising two 8 byte random numbers. These are not ordered
+DESCRIPTION
+  By default, it is the base 32 encoding of a binary encoded string comprising an 8 byte nanosecond precision unix timestamp and an 8 byte random number, in that order. The timestamp prefix allows these IDs to be ordered.
 
-version:  %s
-compiled: %s
-built:    %s
+  Using '-uid' makes it a base 32 encoding of a binary encoded string comprising two 8 byte random numbers. These are not ordered
 
-Usage: %s [option [option]...]
+  The Crockford base32 dictionary used (https://www.crockford.com/base32.html).
 
-Options:
+OPTIONS
+{{.Options}}
+VERSION
+  version:  {{.Version}}
+  compiled: {{.CompiledBy}}
+  built:    {{.BuildTimestamp}}
+
+{{end}}
 `
 
 var (
@@ -35,35 +44,20 @@ var (
 	help      bool
 )
 
-func init() {
-	flag.Usage = func() {
-		fmt.Fprintf(
-			os.Stderr,
-			doc,
-			os.Args[0],
-			buildVersion,
-			compiledBy,
-			buildTimestamp,
-			os.Args[0],
-		)
-		flag.PrintDefaults()
-	}
-
-}
-
 func main() {
-	flag.IntVar(&length, "len", 8, "The number of bytes of randomness to use. Keep in mind that UIDs use double this value for the overall length, while OIDs are this value +8 Bytes for the timestamp.")
-	flag.BoolVar(&unordered, "uid", false, "Generate an 'Unordered' ID (UID) vs the default 'Ordered' ID (OID).")
-	flag.BoolVar(&secure, "secure", false, "Use a cryptographically secure randomness generator.")
-	flag.BoolVar(&hex, "hex", false, "Use a standard HEX (a-f0-9) dictionary vs the default base32 (Crockford) dictionary.")
-	flag.BoolVar(&help, "help", false, "display this message")
-	flag.BoolVar(&help, "h", false, "display this message")
-	flag.Parse()
+	flag.Usage = Usage(Info{
+		Tmpl:           Tmpl,
+		Bin:            "oid",
+		Version:        buildVersion,
+		CompiledBy:     compiledBy,
+		BuildTimestamp: buildTimestamp,
+	})
 
-	if help {
-		flag.Usage()
-		os.Exit(0)
-	}
+	flag.IntVar(&length, "len", 8, "the number of bytes of randomness to use. Keep in mind that UIDs use double this value for the overall length, while OIDs are this value +8 bytes for the timestamp.")
+	flag.BoolVar(&unordered, "uid", false, "generate an unordered ID (UID) vs the default ordered ID (OID).")
+	flag.BoolVar(&secure, "secure", false, "use a cryptographically secure randomness generator.")
+	flag.BoolVar(&hex, "hex", false, "use a standard HEX (a-f0-9) dictionary vs the default base32 (Crockford) dictionary.")
+	flag.Parse()
 
 	source := oid.MathSource
 	if secure {
@@ -83,4 +77,32 @@ func main() {
 		fmt.Println(gen.OID())
 	}
 
+}
+
+// Info represents the infomation used in the default Tmpl string
+type Info struct {
+	Tmpl           string
+	Bin            string
+	Version        string
+	CompiledBy     string
+	BuildTimestamp string
+	Options        string
+}
+
+// Usage wraps a set of `Info` and creates a flag.Usage func
+func Usage(info Info) func() {
+	if len(info.Tmpl) == 0 {
+		info.Tmpl = Tmpl
+	}
+
+	t := template.Must(template.New("manual").Parse(info.Tmpl))
+
+	return func() {
+		var def bytes.Buffer
+		flag.CommandLine.SetOutput(&def)
+		flag.PrintDefaults()
+
+		info.Options = def.String()
+		t.Execute(os.Stdout, info)
+	}
 }
